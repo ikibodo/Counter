513a736b068c04bdfd94790b1a8b58294f7e37b0
ikibodo <kibodo@ya.ru>
2024-07-22 11:21:20 +0200

    Сделано задание Sprint_03 Counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UILabel!
+   
+    @IBOutlet weak var counterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var count: Int = 0
 
-
+    @IBAction func counterButtonTouch(_ sender: Any) {
+        count += 1
+        counterLabel.text = "Значение счётчика: \(count)"
+    }
+    
+    
 }
 
