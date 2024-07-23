f46ba9d86effca9ab6fd7d3b52f24bc68b2b8eb8
ikibodo <kibodo@ya.ru>
2024-07-23 16:12:07 +0200

    Исправления после ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,20 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet weak var counterLabel: UILabel!
-   
-    @IBOutlet weak var counterButton: UIButton!
+    private var count: Int = .zero
+    
+    @IBOutlet weak private var counterLabel: UILabel!
+    
+    @IBOutlet weak private var counterButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    var count: Int = 0
-
-    @IBAction func counterButtonTouch(_ sender: Any) {
+    @IBAction private func counterButtonTouch(_ sender: Any) {
         count += 1
         counterLabel.text = "Значение счётчика: \(count)"
     }
